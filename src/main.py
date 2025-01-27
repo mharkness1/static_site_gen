bae41f87812704e6cb746a035b6a326aa6d1a266
mharkness1 <mharkness.uk@gmail.com>
2025-01-27 22:24:08 +0000

    Update and the creation of the HTML node class needs implemented features

--- src/main.py
+++ src/main.py
@@ -1,10 +1,12 @@
 from textnode import TextNode
-
-print("hello world")
+from htmlnode import HTMLNode
 
 def main():
-    Dummy_test = TextNode('This is a text node', 'bold', 'https://www.boot.dev')
-    print(repr(Dummy_test))
+    #Dummy_test = TextNode('This is a text node', 'bold', 'https://www.boot.dev')
+    #print(repr(Dummy_test))
+    Dummy_HTML_test = HTMLNode('h1','title',None,None)
+    print(repr(Dummy_HTML_test))
+
 
 if __name__ == "__main__":
 	main()
---
+++ src/test_htmlnode.py
@@ -0,0 +1,28 @@
+import unittest
+
+from htmlnode import HTMLNode
+
+
+class TestHTMLNode(unittest.TestCase):
+    def test_eq(self):
+        node = HTMLNode()
+        node2 = HTMLNode()
+        self.assertEqual(node, node2)
+
+    def test_eq(self):
+        node = HTMLNode()
+        node2 = HTMLNode()
+        self.assertEqual(node, node2)
+
+    def test_eq(self):
+        node = HTMLNode()
+        node2 = HTMLNode()
+        self.assertEqual(node, node2)
+
+    def test_eq(self):
+        node = HTMLNode()
+        node2 = HTMLNode()
+        self.assertEqual(node, node2)
+
+if __name__ == "__main__":
+    unittest.main()

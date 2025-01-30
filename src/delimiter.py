104508f8d191bcb9dfb6a423fd1e43a6733e89d1
mharkness1 <mharkness.uk@gmail.com>
2025-01-30 14:29:59 +0000

    Additional delimiter tests and functionality.

--- src/delimiter.py
+++ src/delimiter.py
@@ -1,4 +1,6 @@
-from textnode import TextType
+import re
+
+from textnode import TextType, TextNode
 
 def split_node_delimiter(old_nodes, delimiter, text_type):
     new_nodes = []
@@ -6,6 +8,23 @@ def split_node_delimiter(old_nodes, delimiter, text_type):
     for node in old_nodes:
         if node.text_type != TextType.NORMAL:
             new_nodes.append(node)
+            continue
         
         split_node = node.text.split(delimiter)
-        
+        if len(split_node) % 2 == 0:
+            raise Exception("invalid markdown syntax")
+
+        for i in range(0, len(split_node)):
+            if i % 2 == 0:
+                new_nodes.append(TextNode(split_node[i], TextType.NORMAL))
+            else:
+                new_nodes.append(TextNode(split_node[i], text_type))
+    return new_nodes
+
+def extract_markdown_images(text):
+    matches = re.findall(r"!\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
+    return matches
+
+def extract_markdown_links(text):
+    matches = re.findall(r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
+    return matches
---
+++ src/test_delimiter.py
@@ -0,0 +1,72 @@
+import unittest
+from textnode import TextNode, TextType
+from delimiter import split_node_delimiter
+
+
+class TestDelimiter(unittest.TestCase):
+    def test_delim_basic_delimiter(self):
+        node = TextNode("hello `code` world", TextType.NORMAL)
+        result = split_node_delimiter([node], "`", TextType.CODE)
+        self.assertEqual(len(result), 3)
+        self.assertEqual(result[0].text, "hello ")
+        self.assertEqual(result[0].text_type, TextType.NORMAL)
+        self.assertEqual(result[1].text, "code")
+        self.assertEqual(result[1].text_type, TextType.CODE)
+        self.assertEqual(result[2].text, " world")
+        self.assertEqual(result[2].text_type, TextType.NORMAL)
+
+    def test_delim_multiple_delimiters(self):
+        node = TextNode("hello `code` and `more code`", TextType.NORMAL)
+        result = split_node_delimiter([node], "`", TextType.CODE)
+        self.assertEqual(len(result), 5)
+
+    def test_delim_no_delimiter(self):
+        node = TextNode("just plain text", TextType.NORMAL)
+        result = split_node_delimiter([node], "`", TextType.CODE)
+        self.assertEqual(len(result), 1)
+        self.assertEqual(result[0].text, "just plain text")
+
+    def test_delim_unclosed_delimiter(self):
+        node = TextNode("unclosed `delimiter", TextType.NORMAL)
+        with self.assertRaises(Exception):
+            split_node_delimiter([node], "`", TextType.CODE)
+    
+    def test_delim_bold(self):
+        node = TextNode("this is **bold** text", TextType.NORMAL)
+        result = split_node_delimiter([node], "**", TextType.BOLD)
+        self.assertEqual(len(result),3)
+        self.assertEqual(result[0].text, "this is ")
+        self.assertEqual(result[1].text, "bold")
+        self.assertEqual(result[1].text_type, TextType.BOLD)
+    
+    def test_delim_not_normal(self):
+        node = TextNode("this is text", TextType.ITALIC)
+        result = split_node_delimiter([node], "*", TextType.ITALIC)
+        self.assertEqual(len(result),1)
+        self.assertEqual(result[0].text, "this is text")
+        self.assertEqual(result[0].text_type, TextType.ITALIC)
+
+    def test_delim_list_nodes(self):
+        nodes = [
+                TextNode("hello `code` world", TextType.NORMAL),
+                TextNode("hello `code` world", TextType.NORMAL)
+                ]
+        result = split_node_delimiter(nodes, "`", TextType.CODE)
+        self.assertEqual(len(result), 6)
+
+    def test_delim_no_text_between_case(self):
+        node = TextNode("hello `` world", TextType.NORMAL)
+        result = split_node_delimiter([node], "`", TextType.CODE)
+        self.assertEqual(len(result), 3)
+        self.assertEqual(result[1].text, "")
+
+
+if __name__ == "__main__":
+    unittest.main()
+
+
+
+# Empty text between delimiters
+#node = TextNode("hello `` world", TextType.NORMAL)
+#result = split_node_delimiter([node], "`", TextType.CODE)
+
